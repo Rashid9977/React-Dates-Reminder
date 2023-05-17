679c6f901463fab59bae400349a0c9c6c97fac08
Rashid9977 <119620703+Rashid9977@users.noreply.github.com>
2023-05-17 22:47:02 +0400

    first commit

---
+++ src/data.js
@@ -0,0 +1,27 @@
+export default [
+    {
+        id: 1,
+        title: 'Respublika günü',
+        img: 'https://azertag.az/files/2021/2/1200x630/16220403222093951503_1200x630.jpg',
+        date: '28 May'
+    },
+    {
+        id: 2,
+        title: 'Silahlı qüvvələr günü',
+        img: 'https://cdn.trt.net.tr/images/1af6/0846/ce81/5b310647db181.jpg',
+        date: '26 İyun'
+    },
+    {
+        id: 3,
+        title: 'Bayraq günü',
+        img: 'https://vertikal.az/uploads/posts/2021-11/thumbs/1636199940_d322c23b-0cd7-4ce1-bd9f-1ac5a61a9d1b.jpeg',
+        date: '9 Noyabr'
+    },
+    {
+        id: 4,
+        title: 'Qələbə günü',
+        img: 'https://www.dtx.gov.az/uploads/blog/full/2022/04/23/6263a87592985-1hL7DGY8Bw903Ha.jpg',
+        date: '9 May'
+    }
+
+]
